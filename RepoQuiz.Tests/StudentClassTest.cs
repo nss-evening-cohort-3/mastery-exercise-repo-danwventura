2bfe50f4a2d125d3f035035edafa15df25b52f69
Dan Ventura <danwventura@icloud.com>
2016-10-16 18:42:20 -0500

    added StudentRepo file and created initial migration

---
+++ RepoQuiz.Tests/StudentClassTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepoQuiz.Tests
+{
+    [TestClass]
+    public class StudentClassTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
