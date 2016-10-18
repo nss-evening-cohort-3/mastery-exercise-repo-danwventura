b4c9e86e9c431c398f8828bfcec5b3ccb94240dc
Dan Ventura <danwventura@icloud.com>
2016-10-17 21:25:16 -0500

    added students to database/controller built/displaying in student index view

--- RepoQuiz/Controllers/StudentController.cs
+++ RepoQuiz/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RepoQuiz.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,8 @@ namespace RepoQuiz.Controllers
         // GET: Student
         public ActionResult Index()
         {
+            StudentRepository repo = new StudentRepository();
+            ViewBag.All_Students = repo.GetAllStudents();
             return View();
         }
 
--- RepoQuiz/DAL/NameGenerator.cs
+++ RepoQuiz/DAL/NameGenerator.cs
@@ -13,17 +13,19 @@ namespace RepoQuiz.DAL
         // All methods should be Unit Tested :)
         String[] first_names = new String[] { "Mary", "Susan", "Kim", "Jane", "Stephanie", "Louise", "Rachel", "Amanda", "Austin", "Sam", "John", "Paul", "Chris", "Matt", "David", "Tim", "Luke", "Dan", "Brad", "Zach" };
         String[] last_names = new String[] { "Smith", "Johnson", "Anderson", "Ventura", "Gallagher", "Stevenson", "Martinez", "Xiao", "Fields", "Cuba", "Robinson", "White", "Black", "Green", "Long", "Byram", "Rupp", "Hamill", "Zullo", "McDonald" };
-        String[] majors = new String[] { "English", "Physics", "Pre-Med", "Linguistics", "Political Science", "Hospitality", "Electrical Engineering", "Chemistry", "Finance", "Chinese" };
+        String[] majors = new String[] { "English", "Physics", "Pre-Med", "Linguistics", "Political Science", "Hospitality", "Electrical Engineering", "Chemistry", "Finance", "Chinese", "French", "BioChemistry", "Material Sciences", "Theatre", "History", "Computer Science",  "Japanese", "Asian Studies", "Education", "Pre-Law" };
+
+        Random random_int = new Random();
+        
 
         public Student GenerateStudentData()
         {
-           
+            
 
-            Random random_int = new Random();
-            Random random_int2 = new Random();
+           
             Student new_student = new Student();
-            int random_name_index = random_int.Next(1, 21);
-            int random_major_index = random_int2.Next(1, 11);
+            int random_name_index = random_int.Next(0, 20);
+            int random_major_index = random_int.Next(0, 20);
 
             new_student.FirstName = first_names[random_name_index];
             new_student.LastName = last_names[random_name_index];
